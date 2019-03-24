a816b2e2e408336db1027094d57753dad3e32b2d
Anton Stepanishin <anton@Antons-MacBook-Air.local>
2019-03-24 04:53:05 +0200

    Fix scaling and style switcher style

--- src/initialize/createStyleSwitcher.js
+++ src/initialize/createStyleSwitcher.js
@@ -1,7 +1,7 @@
 export default function (appWrapper) {
 	const switcher = document.createElement('div');
-	appWrapper.style.paddingBottom = '100px';
-	switcher.style.cssText = 'position: fixed; bottom: 0; width: 100%; height: 100px; display: flex; justify-content: center; align-items: center; font-family: sans-serif; z-index: 10; user-select: none; font-weight: 300';
+	appWrapper.style.paddingBottom = '150px';
+	switcher.style.cssText = 'position: fixed; bottom: 0; width: 100%; height: 150px; display: flex; justify-content: center; align-items: center; font-family: sans-serif; z-index: 10; user-select: none; -webkit-user-select: none; font-weight: 300';
 	appWrapper.append(switcher);
 	return switcher;
 }
